1ae30bc2854d7a76c00cc6ef4adb68694090caca
tungnd237 <tungtorres23@gmail.com>
2023-12-27 16:13:47 +0700

    change req

--- test_calculator.py
+++ test_calculator.py
@@ -15,4 +15,6 @@ class TestCalculator:
     
     def	test_multiplication(self):	
         assert	100	==	calculator.multiply(10,	10)	
-	
+
+    def	test_multiplication(self):	
+        assert	5	==	calculator.multiply(25,	5)	
